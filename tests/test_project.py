fa48cf0d720876ed3d7f8d086353cd303972c836
Mathijs van Willigen <not defined>
2020-03-04 19:45:54 +0100

    added dockerfile for githubaction
    Signed-off-by: Mathijs van Willigen <not defined>

---
+++ tests/test_project.py
@@ -0,0 +1,7 @@
+import unittest
+class TestMyProject(unittest.TestCase):
+    def test_my_code_works_properly(self):
+        # Eventually, you would test that your code
+        # actually does, you know, work properly
+        
+        assert True
